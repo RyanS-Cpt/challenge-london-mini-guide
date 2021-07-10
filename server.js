88473a44152ed9cef1a391e47c1344e42a56e937
RyanS-Cpt <ryan.30@icloud.com>
2021-07-10 20:23:08 +0200

    Edit res message

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const express = require("express");
 
 const app = express();
 
-app.get("/",(req,res)=>{
-    res.send("You made it here, well done!");
-})
+app.get("/", (req, res) => {
+	res.send(
+		"You made it here, well done! Please select /pharmacies /colleges /doctors or /hospitals"
+	);
+});
 
 const listener = app.listen(process.env.PORT);
 console.log(listener.address().port);
