ca0a7f1b209c42b29eb2059829952cfe1efac927
RyanS-Cpt <ryan.30@icloud.com>
2021-07-10 21:07:02 +0200

    Complete level 100 and 200

--- server.js
+++ server.js
@@ -2,11 +2,35 @@ const express = require("express");
 
 const app = express();
 
+const stratfordData = require("./data/Stratford.json");
+
+// console.log(stratfordData);
+
 app.get("/", (req, res) => {
 	res.send(
 		"You made it here, well done! Please select /pharmacies /colleges /doctors or /hospitals"
 	);
 });
 
+//Get Pharmacy data
+app.get("/pharmacies", (req, res) => {
+	res.json(stratfordData.pharmacies);
+});
+
+//Get College data
+app.get("/colleges", (req, res) => {
+	res.json(stratfordData.colleges);
+});
+
+//Get Doctor data
+app.get("/doctors", (req, res) => {
+	res.json(stratfordData.doctors);
+});
+
+//Get Hospital data
+app.get("/hospitals", (req, res) => {
+	res.json(stratfordData.hospitals);
+});
+
 const listener = app.listen(process.env.PORT);
-console.log(listener.address().port);
+console.log("Listening on PORT:", listener.address().port);
