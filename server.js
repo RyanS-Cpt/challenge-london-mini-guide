4dbd67ffd0df9d19bcb1ce9e4ad2c46195663773
RyanS-Cpt <ryan.30@icloud.com>
2021-07-10 14:27:52 +0200

    Response message added

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/",(req,res)=>{
-    
+    res.send("You made it here, well done!");
 })
 
 const listener = app.listen(process.env.port);
