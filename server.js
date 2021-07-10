9689d4dd24e1538faa0dd47c6e5ffcded058311b
RyanS-Cpt <74462713+RyanS-Cpt@users.noreply.github.com>
2021-07-10 14:51:29 +0200

    Edit package.json and port address

--- server.js
+++ server.js
@@ -6,5 +6,5 @@ app.get("/",(req,res)=>{
     res.send("You made it here, well done!");
 })
 
-const listener = app.listen(process.env.port);
+const listener = app.listen(process.env.PORT);
 console.log(listener.address().port);
