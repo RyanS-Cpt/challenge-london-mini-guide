3c49c6e807b9f1bc959a187d89d6b6c31b328e55
RyanS-Cpt <ryan.30@icloud.com>
2021-07-11 19:20:25 +0200

    Complete level 500

--- server.js
+++ server.js
@@ -4,41 +4,50 @@ const app = express();
 
 app.get("/", (req, res) => {
 	res.send(
-		"You made it here, well done! Please select :city & /pharmacies, /colleges, /doctors, /hospitals"
+		"You made it here, well done! Please select /:city/:category to retrieve data"
 	);
 });
 
-//Get Pharmacy data
-app.get("/:city/pharmacies", (req, res) => {
-	const { city } = req.params;
+//Get any data from any city
+app.get("/:city/:category", (req, res) => {
+	const city = req.params.city;
+	const category = req.params.category;
 	const capCity = city.charAt(0).toUpperCase() + city.slice(1);
 	const cityData = require(`./data/${capCity}`);
-	res.json(cityData.pharmacies);
+	res.json(cityData[category]);
 });
 
-//Get College data
-app.get("/:city/colleges", (req, res) => {
-	const { city } = req.params;
-	const capCity = city.charAt(0).toUpperCase() + city.slice(1);
-	const cityData = require(`./data/${capCity}`);
-	res.json(cityData.colleges);
-});
+// //Get Pharmacy data
+// app.get("/:city/pharmacies", (req, res) => {
+// 	const { city } = req.params;
+// 	const capCity = city.charAt(0).toUpperCase() + city.slice(1);
+// 	const cityData = require(`./data/${capCity}`);
+// 	res.json(cityData.pharmacies);
+// });
 
-//Get Doctor data
-app.get("/:city/doctors", (req, res) => {
-	const { city } = req.params;
-	const capCity = city.charAt(0).toUpperCase() + city.slice(1);
-	const cityData = require(`./data/${capCity}`);
-	res.json(cityData.doctors);
-});
+// //Get College data
+// app.get("/:city/colleges", (req, res) => {
+// 	const { city } = req.params;
+// 	const capCity = city.charAt(0).toUpperCase() + city.slice(1);
+// 	const cityData = require(`./data/${capCity}`);
+// 	res.json(cityData.colleges);
+// });
 
-//Get Hospital data
-app.get("/:city/hospitals", (req, res) => {
-	const { city } = req.params;
-	const capCity = city.charAt(0).toUpperCase() + city.slice(1);
-	const cityData = require(`./data/${capCity}`);
-	res.json(cityData.hospitals);
-});
+// //Get Doctor data
+// app.get("/:city/doctors", (req, res) => {
+// 	const { city } = req.params;
+// 	const capCity = city.charAt(0).toUpperCase() + city.slice(1);
+// 	const cityData = require(`./data/${capCity}`);
+// 	res.json(cityData.doctors);
+// });
+
+// //Get Hospital data
+// app.get("/:city/hospitals", (req, res) => {
+// 	const { city } = req.params;
+// 	const capCity = city.charAt(0).toUpperCase() + city.slice(1);
+// 	const cityData = require(`./data/${capCity}`);
+// 	res.json(cityData.hospitals);
+// });
 
 const listener = app.listen(process.env.PORT);
 console.log("Listening on PORT:", listener.address().port);
